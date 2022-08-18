0acf602c7d247ee6320379e42cd4153526af4e84
Douglas Miyashita <douglas.miyashita@al.mt.gov.br>
2022-08-18 10:20:20 -0400

    first release.

---
+++ src/Notification/Base.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Linx\OmsNotificationClient\Notification;
+
+use Exception;
+use GuzzleHttp\Client;
+
+class Base
+{
+    /**
+     * @throws Exception
+     */
+    protected function send($url, $token, $body): array
+    {
+        $clientHttp = new Client();
+
+        $head = [
+            'User-Agent' => 'oms-notification-client',
+            'Accept' => 'application/json',
+            'Authorization' => "Bearer $token",
+        ];
+
+        $response = $clientHttp->request(
+            'POST',
+            $url,
+            [
+                'timeout' => 20,
+                'headers' => $head,
+                'json' => $body,
+            ]
+        );
+
+        $result = json_decode($response->getBody(), true);
+
+        return $result;
+    }
+}
---
+++ src/Notification/WebhookService.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace Linx\OmsNotificationClient\Notification;
+
+use Exception;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
+
+class WebhookService extends Base
+{
+    private const ENDPOINT_CREATE = '/api/v1/clients/{clientId}/webhook';
+
+    private const ACCEPTED_METHODS = [
+        'POST',
+        'PUT',
+        'DELETE',
+        'PATCH',
+    ];
+
+    private const ACCEPTED_AUTH_TYPES = [
+        'Bearer',
+        'Basic',
+        'AWS',
+    ];
+
+    private const AVALIABLE_ENV = [
+        'production',
+        'test',
+        'homolog',
+        'development',
+    ];
+
+    /**
+     * @throws Exception
+     */
+    public function create(string $clientId, string $token, array $input, string $env): array
+    {
+        $this->validateInput($clientId, $input);
+
+        $this->validateEnv($env);
+
+        return $this->send($this->getUrl($env, $clientId), $token, $input);
+    }
+
+    private function validateInput(string $clientId, array $input):void
+    {
+        /** @var ValidationValidator $validator */
+        $validator = Validator::make($input, [
+            'clientId' => [
+                'required',
+                'string',
+                Rule::in([$clientId]),
+            ],
+            'referenceId' => [
+                'required',
+                'string'
+            ],
+            'application' => [
+                'required',
+                'string'
+            ],
+            'url' => [
+                'required',
+                'string',
+                'url'
+            ],
+            'method' => [
+                'required',
+                Rule::in(self::ACCEPTED_METHODS),
+            ],
+            'headers' => [
+                'array',
+            ],
+            'retry' => [
+                'integer',
+            ],
+            'auth.type' => [
+                'string',
+                Rule::in(self::ACCEPTED_AUTH_TYPES),
+                Rule::requiredIf(array_key_exists('auth', $input)),
+            ],
+            'auth.token' => [
+                'string',
+                Rule::requiredIf(array_key_exists('auth', $input)),
+            ],
+        ], [
+            'clientId.in' => 'The defined clientId must be equal to ' . $clientId . ', otherwise change the parameter clientId.',
+            'method.in' => 'The selected method is invalid. Options: ' . implode(', ', self::ACCEPTED_METHODS) . '.',
+            'auth.type.in' => 'The selected auth.type is invalid. Options: ' . implode(', ', self::ACCEPTED_AUTH_TYPES) . '.',
+        ])->validate();
+    }
+    
+    private function validateEnv($env): void
+    {
+        /** @var ValidationValidator $validator */
+        $validator = Validator::make(['env' => $env], [
+            'env' => [
+                'string',
+                Rule::in(self::AVALIABLE_ENV),
+            ],
+        ], [
+            'env.in' => 'The selected env is invalid. Options: ' . implode(', ', self::AVALIABLE_ENV) . '.',
+        ])->validate();
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getUrl(string $env, string $clientId): string
+    {
+        $source = dirname(__DIR__).'/../config/oms-notification.php';
+
+        $config = require $source;
+
+        if (null === $config[$env]['host'] || false === filter_var($config[$env]['host'], FILTER_VALIDATE_URL)){
+            throw new Exception("Invalid env config for {$env}.");
+        }
+
+        $path = str_replace('{clientId}', $clientId, self::ENDPOINT_CREATE);
+
+        return $config[$env]['host'].$path;
+    }
+}
